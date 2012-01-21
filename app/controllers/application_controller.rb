acad6ac426c8df253e23d49b868ae0ac4862dbe8
C.R. Myers <myers3.cr@gmail.com>
2012-01-21 13:08:08 -0500

    Cleaned up user index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
-  before_filter :authorize
-  
-  private
-  
-  def authorize
-    unless User.find_by_id(session[:user_id])
-      redirect_to login_url, notice: "Please log in"
-    end
-  end
-  
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  helper_method :current_user
+	protect_from_forgery
+	before_filter :authorize
+
+	private
+
+	def authorize
+		unless User.find_by_id(session[:user_id])
+			redirect_to login_url, notice: "Please log in"
+		end
+	end
+
+	def current_user
+		@current_user ||= User.find(session[:user_id]) if session[:user_id]
+	end
+	helper_method :current_user
 
 end
