ce4b3509f48baf32a7e62670992dbc1356db0fb3
C.R. Myers <myers3.cr@gmail.com>
2012-02-03 21:09:13 -0500

    Updated Gemfile for Rails 3.2. Added pagination to currencies#index

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -2,7 +2,8 @@ class CurrenciesController < ApplicationController
   # GET /currencies
   # GET /currencies.json
   def index
-    @currencies = Currency.order(:iso_code)
+#    @currencies = Currency.order(:iso_code)
+    @currencies = Currency.order(:iso_code).paginate page: params[:page], per_page: 10
 
     respond_to do |format|
       format.html # index.html.erb
