b4aa854b71044a83e4e5f9cc97b119a23466aadf
C.R. Myers <myers3.cr@gmail.com>
2012-06-17 00:21:32 -0400

    moved main navigation to partial that depends on current_user_type updated current_user_type to be lower case

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
 	before_filter :authorize
+	helper_method :current_user, :current_user_type
 
 	private
 
@@ -13,6 +14,9 @@ class ApplicationController < ActionController::Base
 	def current_user
 		@current_user ||= User.find(session[:user_id]) if session[:user_id]
 	end
-	helper_method :current_user
+
+  def current_user_type
+    current_user.org.org_type.name.downcase
+  end
 
 end
