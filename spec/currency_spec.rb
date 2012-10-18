857da698966c9d675556ed904ee2c66888d5a7aa
C.R. Myers <myers3.cr@gmail.com>
2012-10-17 22:10:37 -0400

    converted to FactoryGirl simplified syntax

--- spec/currency_spec.rb
+++ spec/currency_spec.rb
@@ -3,53 +3,52 @@ require 'spec_helper'
 describe Currency do
 
   it "has a valid factory" do
-    FactoryGirl.create(:currency).should be_valid
+    create(:currency).should be_valid
   end
   
   it "is invalid without an ISO code" do
-    FactoryGirl.build(:currency, iso_code: nil).should_not be_valid
+    build(:currency, iso_code: nil).should_not be_valid
   end
   
   it "is invalid without a description" do
-    FactoryGirl.build(:currency, description: nil).should_not be_valid
+    build(:currency, description: nil).should_not be_valid
   end
   
   it "is invalid with numbers for ISO code" do
-    FactoryGirl.build(:currency, iso_code: "123").should_not be_valid
+    build(:currency, iso_code: "123").should_not be_valid
   end
   
   it "is invalid with special characters for ISO code" do
-    FactoryGirl.build(:currency, iso_code: "$@*").should_not be_valid
+    build(:currency, iso_code: "$@*").should_not be_valid
   end
 
   it "is invalid with 2 letters for ISO code" do
-    FactoryGirl.build(:currency, iso_code: "ab").should_not be_valid
+    build(:currency, iso_code: "ab").should_not be_valid
   end
 
   it "is valid with 3 letters for ISO code" do
-    FactoryGirl.build(:currency, iso_code: "abc").should be_valid
+    build(:currency, iso_code: "abc").should be_valid
   end
   
   it "is invalid with 4 letters for ISO code" do
-    FactoryGirl.build(:currency, iso_code: "abcd").should_not be_valid
+    build(:currency, iso_code: "abcd").should_not be_valid
   end
   
   it "is invalid with a duplicate ISO code" do
-    FactoryGirl.create(:currency, iso_code: 'abc')
-    FactoryGirl.build(:currency, iso_code: 'abc').should_not be_valid
+    create(:currency, iso_code: 'abc')
+    build(:currency, iso_code: 'abc').should_not be_valid
   end
     
-  
   it "is valid with a 32 character description" do
-    FactoryGirl.build(:currency, description: "12345678901234567890123456789012").should be_valid
+    build(:currency, description: "12345678901234567890123456789012").should be_valid
   end
   
   it "is invalid with a 33 character description" do
-      FactoryGirl.build(:currency, description: "123456789012345678901234567890123").should_not be_valid
+      build(:currency, description: "123456789012345678901234567890123").should_not be_valid
   end
   
   it "converts lowercase ISO to uppercase when saved" do
-    f = FactoryGirl.create(:currency, iso_code: "abc")
+    f = create(:currency, iso_code: "abc")
     f.iso_code.should == "ABC"
   end
 
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Message do
 
   it "has a valid factory" do
-    FactoryGirl.create(:message).should be_valid
+    create(:message).should be_valid
   end
 
 end
