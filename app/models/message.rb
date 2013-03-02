64daf60ad0ef9db9ff14a72ad01295ea0eda2a2b
C.R. Myers <myers3.cr@gmail.com>
2013-03-02 11:22:36 -0500

    cleaned up message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,12 +4,12 @@ class Message < ActiveRecord::Base
 
 	validates_presence_of :headline
 	validates_length_of :headline, :maximum => 64
+  validates_presence_of :content
+  validates_presence_of :start_date
+  validates_presence_of :end_date
+	
+	def self.current
+	  where("start_date <= ? and end_date >= ?", Time.now, Time.now)
+  end
 
-	validates_presence_of :content
-
-	validates_presence_of :start_date
-
-	validates_presence_of :end_date
-
-	scope :current, where("start_date <= ? and end_date >= ?", Time.now, Time.now)
 end
